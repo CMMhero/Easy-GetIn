b07dddc18683d3b1d0a603aab80632160b55c1f6
CMMhero <ch.mh.marvelio@gmail.com>
2022-11-16 18:00:22 +0700

    logout button

--- app/src/main/java/id/ac/umn/easygetin/activity_home.java
+++ app/src/main/java/id/ac/umn/easygetin/activity_home.java
@@ -2,7 +2,12 @@ package id.ac.umn.easygetin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class activity_home extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class activity_home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+        Toast.makeText(activity_home.this, "Successfully logged out", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(activity_home.this, activity_main.class));
+        finish();
     }
 }
