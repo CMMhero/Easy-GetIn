beb3a517cc64ae601c56b714511384ee352a3039
CMMhero <ch.mh.marvelio@gmail.com>
2022-12-03 17:21:43 +0700

    activities

---
+++ app/src/main/java/id/ac/umn/easygetin/ProfileActivity.java
@@ -0,0 +1,73 @@
+package id.ac.umn.easygetin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    TextView username, email;
+    Button save;
+    FirebaseUser user;
+    ImageView mobilImage;
+    StorageReference storageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+        userId = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
+
+        username = findViewById(R.id.usernameTV);
+        email = findViewById(R.id.emailTV);
+        mobilImage = findViewById(R.id.mobilImageView);
+        save = findViewById(R.id.save);
+    }
+
+    DocumentReference documentReference = fStore.collection("users").document(userId);
+
+    public void uploadImage(Uri imageUri) {
+        StorageReference fileRef = storageRef.child("profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(ProfileActivity.this, "Updated Profile Picture", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ProfileActivity.this, "Fail to update Profile Picture", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void save(View view) {
+    }
+}
